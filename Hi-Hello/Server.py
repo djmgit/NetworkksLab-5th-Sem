88a0fba2463acc20d6b4e5874d02c03703fe2a21
Deepjyoti Mondal <djmdeveloper060796@gmail.com>
2016-08-07 21:28:16 +0530

    Implemented a simple server-client program

---
+++ Hi-Hello/Server.py
@@ -0,0 +1,22 @@
+import socket
+sock=socket.socket()
+# getting host name
+host=socket.gethostname()
+port=12345
+# binding port to server
+sock.bind((host,port))
+sock.listen(5)	
+print 'Server started, listening at host: '+str(host)+' port: '+str(port)
+while True:
+	# accepting connection
+	con,addr=sock.accept()
+	print 'got connection from '+str(addr)
+	# receiving message
+	mesg=con.recv(1024)
+	# sending message
+	con.send("Hello, I am Server!!")
+	# closing connection
+	con.close()
+	
+
+print 'Server terminated'
